74c8c52832a9a7345b2fe1582cf03f6f1870553f
aokihu <aokihu@gmail.com>
2023-01-08 23:22:08 +0800

    增加title和viewport属性

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./global.css";
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <title>Password Card</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <body>{children}</body>
     </html>
   )
