81dc2a2a9f62061b19db9935bd3be7a71f16ca1c
aokihu <aokihu@gmail.com>
2023-01-08 22:43:42 +0800

    完成草稿

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
+import {  customAlphabet } from "nanoid";
+
+const nanoid =  customAlphabet('0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@#_')
+
+const PasswordZone = () => {
+  return (
+    <div className="password-card__zone">
+      <div>{nanoid(4)}</div>
+      <div>{nanoid(4)}</div>
+    </div>
+  );
+};
+
 export default function Page() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div className="password-card-container">
+      <div className="password-card__head">
+        <div className="password-card__logo">LOGO</div>
+        <div className="password-card__account">
+          Account
+        </div>
+      </div>
+      <div className="password-card__body">
+        <div className="password-card__zones">
+          <PasswordZone />
+          <PasswordZone />
+          <PasswordZone />
+        </div>
+      </div>
+    </div>
+  );
 }
