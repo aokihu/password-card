034bf43496b0dace2be0fdf485caec71691f2d62
aokihu <aokihu@gmail.com>
2023-01-08 23:38:55 +0800

    增加在手机上旋转显示

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@ import PasswordZone from "./PasswordZone";
 
 export default function Page() {
   return (
-    <div className="password-card-container">
-      <div className="password-card__head">
-        <div className="password-card__logo">LOGO</div>
-        <div className="password-card__account">
-          Account
+    <div className="password-card__warp">
+      <div className="password-card__container">
+        <div className="password-card__head">
+          <div className="password-card__logo">LOGO</div>
+          <div className="password-card__account">
+            Account
+          </div>
         </div>
-      </div>
-      <div className="password-card__body">
-        <div className="password-card__zones">
-          <PasswordZone />
-          <PasswordZone />
-          <PasswordZone />
+        <div className="password-card__body">
+          <div className="password-card__zones">
+            <PasswordZone />
+            <PasswordZone />
+            <PasswordZone />
+          </div>
         </div>
       </div>
     </div>
